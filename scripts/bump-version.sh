7d905130525465d6cce5b8a7b79c345e89ce69f9
Kamil Ogórek <kamil.ogorek@gmail.com>
2022-02-24 14:53:00 +0100

    ci: Add release workflow for Craft automation (#25)

---
+++ scripts/bump-version.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -euo pipefail
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $SCRIPT_DIR/..
+
+OLD_VERSION="${1}"
+NEW_VERSION="${2}"
+
+echo "Current version: ${OLD_VERSION}"
+echo "Bumping version: ${NEW_VERSION}"
+
+# Bump version in pubspec.yaml
+perl -pi -e "s/^version: .*/version: $NEW_VERSION/" pubspec.yaml
