e7620f098c89a21b8cfecc24d7a2acc19a2fd506
Ivan Dlugos <dlugos.ivan@gmail.com>
2022-06-27 10:24:13 +0200

    chore: use sentry-cli.properties in the download script
    to enable future automatic update PRs

--- scripts/download-sentry-cli.sh
+++ scripts/download-sentry-cli.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 cd $(dirname "$0")
-REPO=getsentry/sentry-cli
-VERSION=1.69.1
-PLATFORMS="Darwin-x86_64 Linux-i686 Linux-x86_64 Windows-i686"
 
-rm -f lib/assets/sentry-cli-*
+props_file="sentry-cli.properties"
+
+function prop {
+  grep "$1" $props_file | cut -d'=' -f2 | xargs
+}
+
+base_url="$(prop 'repo')/releases/download/$(prop 'version')"
+target_dir="../lib/assets/"
+PLATFORMS="Darwin-universal Linux-i686 Linux-x86_64 Windows-i686"
+
+rm -f $target_dir/sentry-cli-*
 for plat in $PLATFORMS; do
   suffix=''
   if [[ $plat == *"Windows"* ]]; then
     suffix='.exe'
   fi
   echo "${plat}"
-  download_url=https://github.com/$REPO/releases/download/$VERSION/sentry-cli-${plat}${suffix}
-  fn="lib/assets/sentry-cli-${plat}${suffix}"
+  download_url=$base_url//sentry-cli-${plat}${suffix}
+  fn="$target_dir/sentry-cli-${plat}${suffix}"
   curl -SL --progress-bar "$download_url" -o "$fn"
   chmod +x "$fn"
 done
