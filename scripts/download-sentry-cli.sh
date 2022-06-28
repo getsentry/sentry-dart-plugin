863905a279a67ed1a57a48b3f46cf6e8c3d71d38
Ivan Dlugos <6349682+vaind@users.noreply.github.com>
2022-06-28 09:23:36 +0200

    Refactor: CLI binaries integration (#36)

---
+++ scripts/download-sentry-cli.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+cd $(dirname "$0")
+
+props_file="sentry-cli.properties"
+
+function prop {
+  grep "$1" $props_file | cut -d'=' -f2 | xargs
+}
+
+base_url="$(prop 'repo')/releases/download/$(prop 'version')"
+target_dir="../lib/assets/"
+PLATFORMS="Darwin-universal Linux-i686 Linux-x86_64 Windows-i686"
+
+rm -f $target_dir/sentry-cli-*
+for plat in $PLATFORMS; do
+  suffix=''
+  if [[ $plat == *"Windows"* ]]; then
+    suffix='.exe'
+  fi
+  echo "${plat}"
+  download_url=$base_url//sentry-cli-${plat}${suffix}
+  fn="$target_dir/sentry-cli-${plat}${suffix}"
+  curl -SL --progress-bar "$download_url" -o "$fn"
+  chmod +x "$fn"
+done
