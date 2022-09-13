e1d7998da449d343c14aa15298e6a18a7c57f689
Denis Andrasec <denrase@gmail.com>
2022-09-13 15:52:11 +0200

    integration test with sample project

---
+++ integration-test/integration-test-server-start.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+python3 integration-test/integration-test-server.py
---
+++ integration-test/project/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ProjectApp());
+}
+
+class ProjectApp extends StatelessWidget {
+  const ProjectApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sample Project',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ProjectWidget(),
+    );
+  }
+}
+
+class ProjectWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Project"),
+      ),
+      body: Center(
+        child: Text(
+          'Sample Project',
+        )
+      )
+    );
+  }
+}
