4e37dc379d6433db406565f9ffcce8490323bb04
Ivan Dlugos <dlugos.ivan@gmail.com>
2022-10-06 19:34:57 +0200

    test: add more plugin tests

--- test/plugin_test.dart
+++ test/plugin_test.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 import 'dart:convert';
 
+import 'package:file/file.dart';
+import 'package:file/memory.dart';
 import 'package:process/process.dart';
 import 'package:test/test.dart';
 
@@ -10,19 +12,50 @@ import 'package:sentry_dart_plugin/src/utils/injector.dart';
 void main() {
   final plugin = SentryDartPlugin();
   late MockProcessManager pm;
+  late FileSystem fs;
 
   setUp(() {
-    // override process manager
+    // override dependencies for testing
     pm = MockProcessManager();
     injector.registerSingleton<ProcessManager>(() => pm, override: true);
+    fs = MemoryFileSystem.test();
+    injector.registerSingleton<FileSystem>(() => fs, override: true);
   });
 
-  test('fails without args', () async {
+  test('fails without args and pubspec', () async {
     final exitCode = await plugin.run([]);
     expect(exitCode, 1);
     expect(pm.commandLog, const [
       'chmod +x .dart_tool/pub/bin/sentry_dart_plugin/sentry-cli',
       '.dart_tool/pub/bin/sentry_dart_plugin/sentry-cli help'
+    ]);
+  });
+
+  test('works with pubspec', () async {
+    fs.file('pubspec.yaml').writeAsStringSync('''
+name: project
+version: 1.1.0
+
+sentry:
+  upload_native_symbols: true
+  include_native_sources: true
+  upload_source_maps: true
+  auth_token: t
+  project: p
+  org: o
+  url: http://127.0.0.1
+  log_level: debug
+''');
+    final exitCode = await plugin.run([]);
+    expect(exitCode, 0);
+    expect(pm.commandLog, const [
+      'chmod +x .dart_tool/pub/bin/sentry_dart_plugin/sentry-cli',
+      '.dart_tool/pub/bin/sentry_dart_plugin/sentry-cli help',
+      '.dart_tool/pub/bin/sentry_dart_plugin/sentry-cli --url http://127.0.0.1 --auth-token t --log-level debug upload-dif --include-sources --org o --project p /',
+      '.dart_tool/pub/bin/sentry_dart_plugin/sentry-cli --url http://127.0.0.1 --auth-token t --log-level debug releases --org o --project p new project@1.1.0',
+      '.dart_tool/pub/bin/sentry_dart_plugin/sentry-cli --url http://127.0.0.1 --auth-token t --log-level debug releases --org o --project p files project@1.1.0 upload-sourcemaps //build/web --ext map --ext js',
+      '.dart_tool/pub/bin/sentry_dart_plugin/sentry-cli --url http://127.0.0.1 --auth-token t --log-level debug releases --org o --project p files project@1.1.0 upload-sourcemaps / --ext dart',
+      '.dart_tool/pub/bin/sentry_dart_plugin/sentry-cli --url http://127.0.0.1 --auth-token t --log-level debug releases --org o --project p finalize project@1.1.0'
     ]);
   });
 }
