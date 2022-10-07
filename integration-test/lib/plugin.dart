95ce43ed5ce7b627aec64b15e7ffa39e3c78eef5
Ivan Dlugos <dlugos.ivan@gmail.com>
2022-10-07 10:18:23 +0200

    chore: update integration test docs & how it's called

--- integration-test/lib/plugin.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:async';
-
-import 'package:sentry_dart_plugin/sentry_dart_plugin.dart';
-
-/// Main class that executes the SentryDartPlugin
-FutureOr<void> main(List<String> arguments) async {
-  await SentryDartPlugin().run(arguments);
-}
