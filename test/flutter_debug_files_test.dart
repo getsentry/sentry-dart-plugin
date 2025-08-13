ced1eee38f32bd8581f40facff8df8f02a1f1d4b
Giancarlo Buenaflor <giancarlobuenaflor97@gmail.com>
2025-08-13 22:31:53 +0200

    Update

--- test/flutter_debug_files_test.dart
+++ test/flutter_debug_files_test.dart
@@ -1,8 +1,8 @@
 import 'package:file/memory.dart';
 import 'package:test/test.dart';
 
-import 'package:sentry_dart_plugin/src/symbol_maps/dart_symbol_map_debug_file_collector.dart';
 import 'package:sentry_dart_plugin/src/configuration.dart';
+import 'package:sentry_dart_plugin/src/symbol_maps/dart_symbol_map_discovery.dart';
 
 void main() {
   group('collectDebugFilesForDartMap', () {
